59475c56496364c7f8a006c40b9e5c3b1aabb9be
LawlietBlack <kenneth.black@live.com>
2016-06-02 14:10:37 -0700

    add Child Functionality, Improved UX, Finished MVP

--- src/main/resources/static/js/tree.js
+++ src/main/resources/static/js/tree.js
@@ -30,8 +30,6 @@ function newPerson(name, gender, callback) {
     });
 }
 
-
-
 function updatePerson(id, person) {
     $.ajax({
         type: 'PUT',
@@ -114,9 +112,42 @@ function newMarriage(person, spouse, childrenIds) {
     })
 }
 
+function addChild(marriage, child) {
+    var newChildren = marriage.children.slice();
+    newChildren.push(child);
+    var newMarriage = {
+        "spouse": domain + "/api/people/" + marriage.spouse.id,
+        "children": newChildren.map(function(child) {
+            return domain + "/api/people/" + child.id;
+        })
+    };
+    console.log(newMarriage);
+    $.ajax({
+        type: 'PUT',
+        url: '/api/marriages/' + marriage.id,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(newMarriage),
+        dataType: 'json',
+        success: function(savedMarriage) {
+            console.log(savedMarriage);
+            updateTreeList(tree, child);
+        }
+    })
+}
+
+function isMarried(person) {
+    if(person.marriages.length > 0) return true;
+    for(var i=0; i<people.length; i++) {
+        if(people[i].marriages.length > 0 && people[i].marriages[0].spouse.id === person.id) return true;
+    }
+    return false;
+}
+
 var url = window.location.href;
 var domain = window.location.href.split("/").slice(0, 3).join("/");
-console.log(domain);
 var people, tree;
 var treeId = location.pathname.split("/").pop();
 $(document).ready(function() {
@@ -124,14 +155,13 @@ $(document).ready(function() {
     $.getJSON("/data/trees/" + treeId, function(data) {
         tree = data;
         people = data.people;
-        console.log(data);
         var config = {
             height: 600,
             width: 800,
             nodeWidth: 100,
             callbacks: {
                 textRenderer: function(name, extra, textClass) {
-                    return "<p align='center' class='" + textClass + "'>" + name + "</p><button class='add-marriage cyan'>+</button><p class='tooltip'>Add Marriage</p>";
+                    return "<p align='center' class='" + textClass + "'>" + name + "</p><button class='add-marriage add-person pink'>+</button><p class='tooltip'>Add Marriage</p><button class='add-child add-person cyan'>+</button><p class='tooltip'>Add Child</p>";
                 }
             }
         };
@@ -140,20 +170,42 @@ $(document).ready(function() {
 });
 var personName, person;
 $('body').on('mouseenter', '.node', function(e) {
-   $(e.target).children("button").show();
+    var hoverName = $(e.target).children(".nodeText").html();
+    var hoverPerson = getPersonByName(people, hoverName);
+    if(hoverPerson.marriages.length !== 0) $(e.target).children(".add-child").show();
+    if(!isMarried(hoverPerson)) $(e.target).children(".add-marriage").show();
+
 }).on('mouseleave', '.node', function(e) {
     $(e.target).children("button").hide();
-}).on('mouseenter', '.add-marriage', function(e) {
+
+}).on('mouseenter', '.add-person', function(e) {
     $(e.target).next(".tooltip").show();
-}).on('mouseleave', '.add-marriage', function(e) {
+
+}).on('mouseleave', '.add-person', function(e) {
     $(e.target).next(".tooltip").hide();
+
 }).on('click', '.add-marriage', function(e) {
     personName = $(e.target).prev().html();
     person = getPersonByName(people, personName);
+    if(isMarried(person)) return;
+    
     $('#related-person').text(personName);
     $('#addMarriageModal').openModal();
-}).on('click', '.add-confirm', function(e) {
+
+}).on('click', '.add-child', function(e) {
+    personName = $(e.target).prev().prev().prev().html();
+    person = getPersonByName(people, personName);
+    if(person.marriages.length === 0) return;
+    $('#first-parent').text(personName);
+    $('#second-parent').text(person.marriages[0].spouse.name);
+    $('#addChildModal').openModal();
+
+}).on('click', '#add-marriage-submit', function(e) {
     newPerson($("#spouseName").val(), $("#spouseGender").val(), function(spouse) {
         newMarriage(person, spouse);
+    });
+}).on('click', '#add-child-submit', function(e) {
+    newPerson($("#childName").val(), $("#childGender").val(), function(child) {
+        addChild(person.marriages[0], child);
     });
 });
