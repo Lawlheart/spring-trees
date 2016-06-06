2c1e46b953d2fbe423e866d6d57c3a1cd928eb04
LawlietBlack <kenneth.black@live.com>
2016-06-06 08:40:06 -0700

    configure postgres database

--- src/main/java/com/lawlietblack/springtrees/configuration/WebConfiguration.java
+++ src/main/java/com/lawlietblack/springtrees/configuration/WebConfiguration.java
@@ -1,16 +1,15 @@
 package com.lawlietblack.springtrees.configuration;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class WebConfiguration {
-    @Bean
-    ServletRegistrationBean h2servletRegistration() {
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-        registrationBean.addUrlMappings("/console/*");
-        return registrationBean;
-    }
+//    @Bean
+//    ServletRegistrationBean h2servletRegistration() {
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+//        registrationBean.addUrlMappings("/console/*");
+//        return registrationBean;
+//    }
 }
